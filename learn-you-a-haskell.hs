087ba0431a62ee3d80325ab63915c794614bfde1
tuckpuck <tuckertriggs@gmail.com>
2022-01-29 09:05:26 +0300

    add gchi intructions

---
+++ learn-you-a-haskell.hs
@@ -0,0 +1,22 @@
+-- Open Haskell compiler
+-- gchi
+
+-- Open this file in gchi compiler
+-- ghci learn-you-a-haskell.hs
+
+-- Open a program in ghci compiler
+-- ghci src/lib.hs
+
+-- Load a file when already in the ghci compiler 
+-- :l hello.hs
+
+-- Reload Haskell compiler
+-- :r
+
+-- Quit gchi
+-- :q or :quit
+
+
+
+
+
