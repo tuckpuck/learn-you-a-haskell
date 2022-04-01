dcb665c1768e7a0905a29278786b3bf8c21d2582
tuckpuck <tuckertriggs@gmail.com>
2022-04-01 20:02:18 +0200

    Improve Shape data type with an intermediate Point data type. Add Nudge function

--- learn-you-a-haskell.hs
+++ learn-you-a-haskell.hs
@@ -1122,8 +1122,8 @@ showSphereVolume = Sphere.sphereVolume 24.55
 -- Everything before the equal sign is the type, and after the equal sign are the value constructors. 
 -- Both the type name and and value constructors start with an uppercase letter.
 
--- Here we make a shape data type:
--- Deriving show is added to a allow it to print
+-- Here we make a Shape data type:
+-- Deriving show is added to allow it to print
 data Shape = Circle Float Float Float | Rectangle Float Float Float Float
     deriving (Show)
 
@@ -1137,3 +1137,23 @@ findRectArea = area (Rectangle 0 0 100 100)
 -- Value constructors are functions and can be mapped and partially applied
 -- Here is an example of creating concentric circles with different radii
 concentricCircles = map (Circle 10 10) [2,4,6,8]
+
+-- Improve our Shape type by adding intermediate Point data type
+data Point = Point Float Float 
+    deriving (Show)
+data Shape' = Circle' Point Float | Rectangle' Point Point
+    deriving (Show)
+
+area' :: Shape' -> Float
+area' (Circle' _ r) = pi * r ^ 2
+area' (Rectangle' (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+findCircleArea' = area (Circle 10 20 10)
+findRectArea' = area (Rectangle 0 0 100 100)
+
+-- Create a function to nudge a shape
+nudge :: Shape' -> Float -> Float -> Shape'
+nudge (Circle' (Point x y) r) a b = Circle' (Point (x + a) (y + b)) r
+nudge (Rectangle' (Point x1 y1) (Point x2 y2)) a b = Rectangle' (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
+
+tryNudge = nudge (Circle' (Point 34 34) 10) 5 10
